166805c67d61d1a9ea7261393840678b02aae535
Jan Poeschko <jpoeschko@wolfram.com>
2018-11-16 20:32:59 +0100

    hook up rollup-plugin-node-resolve so the import of promise-polyfill actually works

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
     input: 'src/main.js',
@@ -24,6 +25,7 @@ export default {
                     }
                 }]
             ]
-        })
+        }),
+        resolve()
     ]
 };
