1217400aaa0ddc27f94ba926db6818b3c4181b7f
Jan Poeschko <jpoeschko@wolfram.com>
2018-11-16 20:28:27 +0100

    install a polyfill for Promise

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+import PromisePolyfill from 'promise-polyfill';
+
+const globalNS = (function() {
+    if (typeof self !== 'undefined') {
+        return self;
+    }
+    if (typeof window !== 'undefined') {
+        return window;
+    }
+    if (typeof global !== 'undefined') {
+        return global;
+    }
+    throw new Error('Unable to locate global object');
+})();
+
+const Promise = globalNS.Promise || PromisePolyfill;
+
 const installedScripts = {};
 const libraryLoading = {};
 let counter = 0;
