e744a3da63d953943e091b53a614a060e5b82b32
Jan Poeschko <jpoeschko@wolfram.com>
2019-06-28 11:10:45 +0200

    use Docusaurus to generate a static website for the project

---
+++ website/core/Footer.js
@@ -0,0 +1,72 @@
+const React = require('react');
+
+class Footer extends React.Component {
+  docUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    const docsUrl = this.props.config.docsUrl;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    return `${baseUrl}${docsPart}${langPart}${doc}`;
+  }
+
+  pageUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    return baseUrl + (language ? `${language}/` : '') + doc;
+  }
+
+  render() {
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <a href={this.props.config.baseUrl} className="nav-home">
+            {this.props.config.footerIcon && (
+              <img
+                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                alt={this.props.config.title}
+                width="66"
+                height="58"
+              />
+            )}
+          </a>
+          <div>
+            <h5>Docs</h5>
+            <a href={this.docUrl('GettingStarted.html', this.props.language)}>
+              Getting Started
+            </a>
+          </div>
+          <div>
+            <h5>Community</h5>
+            <a href={this.pageUrl('users.html', this.props.language)}>
+              User Showcase
+            </a>
+            <a
+              href="https://community.wolfram.com/content?curTag=wolfram%20cloud"
+              target="_blank"
+              rel="noreferrer noopener">
+              Wolfram Community
+            </a>
+          </div>
+          <div>
+            <h5>More</h5>
+            {/*<a href={`${this.props.config.baseUrl}blog`}>Blog</a>*/}
+            <a href="https://github.com/WolframResearch/wolfram-notebook-embedder">GitHub</a>
+            <a
+              className="github-button"
+              href={this.props.config.repoUrl}
+              data-icon="octicon-star"
+              data-count-href="/WolframResearch/wolfram-notebook-embedder/stargazers"
+              data-show-count="true"
+              data-count-aria-label="# stargazers on GitHub"
+              aria-label="Star this project on GitHub">
+              Star
+            </a>
+          </div>
+        </section>
+
+        <section className="copyright">{this.props.config.copyright}</section>
+      </footer>
+    );
+  }
+}
+
+module.exports = Footer;
