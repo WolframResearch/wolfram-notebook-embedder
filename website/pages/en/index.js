6e28224000c6dfa1d5209467522a115225a0bf4d
Jan Poeschko <jpoeschko@wolfram.com>
2019-07-26 14:04:16 +0200

    do not repeat logo on landing page

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -22,12 +22,6 @@ class HomeSplash extends React.Component {
       </div>
     );
 
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
-
     const ProjectTitle = () => (
       <h2 className="projectTitle">
         {siteConfig.title}
@@ -53,7 +47,6 @@ class HomeSplash extends React.Component {
 
     return (
       <SplashContainer>
-        <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
